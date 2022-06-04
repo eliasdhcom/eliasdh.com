3de643523a385724f36d98b011e84b622c3bc03d
EliasDeHondt <Elias.dehondt@outlook.com>
2022-06-04 15:02:39 +0200

    version-0.3

---
+++ cloud-server/config/config.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocsm81zg7k3h',
+  'passwordsalt' => 'Fa50OztDQ7m0CLPR2sDcoA3LYO49Y2',
+  'secret' => 'g9jdsoAsu3aUzvRSX/zWAt+fvGSL4LeJhHgVIR7l6G0EKMxq',
+  'trusted_domains' => 
+  array (
+    0 => 'eliasdehondt.com',
+  ),
+  'theme' => 'tux',
+  'default_phone_region' => 'BE',
+  'datadirectory' => '/var/www/root/cloud-server/data',
+  'dbtype' => 'mysql',
+  'version' => '21.0.1.1',
+  'overwrite.cli.url' => 'https://eliasdehondt.com/cloud-server',
+  'dbname' => 'cloudserver',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 's2',
+  'dbpassword' => 'AZERTYUIOP123456789ROOT',
+  'installed' => true,
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+);
